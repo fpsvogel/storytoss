047dd13d2d1c035ddcac7837679ac2e73191198d
Felipe Vogel <fps.vogel@gmail.com>
2021-12-23 12:02:30 -0500

    Add spacer lines in User spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,6 +20,7 @@ RSpec.describe User, type: :model do
 
     context "when the password is too short" do
       let!(:user) { build_stubbed(:user, password: "ab12!") }
+
       it "is invalid" do
         message = "is too short (minimum is 6 characters)"
         expect(user).to_not be_valid
@@ -30,6 +31,7 @@ RSpec.describe User, type: :model do
     context "when the password confirmation does not match the password" do
       let!(:user) { build_stubbed(:user, password: "qwerty",
                                         password_confirmation: "azerty") }
+
       it "is invalid" do
         message = "doesn't match Password"
         expect(user).to_not be_valid
@@ -39,6 +41,7 @@ RSpec.describe User, type: :model do
 
     context "when the username contains nonalphanumeric, non-hyphen characters" do
       let!(:user) { build_stubbed(:user, username: "some_user") }
+
       it "is invalid" do
         message = "must include only lowercase letters, numbers, and hyphens"
         expect(user).to_not be_valid
@@ -48,6 +51,7 @@ RSpec.describe User, type: :model do
 
     context "when the username contains capital letters" do
       let!(:user) { build_stubbed(:user, username: "User123") }
+
       it "is invalid" do
         message = "must include only lowercase letters, numbers, and hyphens"
         expect(user).to_not be_valid
@@ -57,6 +61,7 @@ RSpec.describe User, type: :model do
 
     context "when the username is too long" do
       let!(:user) { build_stubbed(:user, username: "a" * 31) }
+
       it "is invalid" do
         message = "is too long (maximum is 30 characters)"
         expect(user).to_not be_valid
@@ -67,6 +72,7 @@ RSpec.describe User, type: :model do
     context "when the username is not unique" do
       let!(:user1) { create(:user, username: "user") }
       let!(:user2) { build_stubbed(:user, username: "user") }
+
       it "is invalid" do
         message = "has already been taken"
         expect(user2).to_not be_valid
@@ -76,6 +82,7 @@ RSpec.describe User, type: :model do
 
     context "when the email address is invalid" do
       let!(:user) { build_stubbed(:user, email: "user (at) example.com") }
+
       it "is invalid" do
         message = "must be a valid email address"
         expect(user).to_not be_valid
@@ -85,6 +92,7 @@ RSpec.describe User, type: :model do
 
     context "when the email address is too long" do
       let!(:user) { build_stubbed(:user, email: "#{"a" * (256 - 12)}@example.com") }
+
       it "is invalid" do
         message = "is too long (maximum is 255 characters)"
         expect(user).to_not be_valid
@@ -95,6 +103,7 @@ RSpec.describe User, type: :model do
     context "when the email address is not unique" do
       let!(:user1) { create(:user, email: "user@example.com") }
       let!(:user2) { build_stubbed(:user, email: "user@example.com") }
+
       it "is invalid" do
         message = "has already been taken"
         expect(user2).to_not be_valid
