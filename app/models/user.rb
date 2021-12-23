60714e172d576014751625ea4c0be9f45ae1c312
Felipe Vogel <fps.vogel@gmail.com>
2021-12-23 09:08:17 -0500

    Add User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,25 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
 
   validates :password,
+            presence: true,
+            length: { minimum: 6 },
             confirmation: true,
             if: -> { new_record? || changes[:crypted_password] }
+  validates :password_confirmation,
+            presence: true,
+            if: -> { new_record? || changes[:crypted_password] }
+
+  validates :username,
+            presence: true,
+            uniqueness: true,
+            length: { maximum: 30 },
+            format: { with: /\A[a-z0-9\-\s]+\z/,
+              message: "must include only lowercase letters, numbers, and hyphens" }
+
+  validates :email,
+            presence: true,
+            uniqueness: true,
+            length: { maximum: 255 },
+            format: { with: /\A[^@\s]+@[^@\s]+\z/,
+                      message: "must be a valid email address" }
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,7 @@ FactoryBot.define do
   factory :user do
     username { "user123" }
     email { "user@example.jp" }
+    password { "qwerty" }
+    password_confirmation { password || "qwerty" }
   end
 end
