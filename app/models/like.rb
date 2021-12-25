ed23b79eb1062b39e5cfaf024a6deafe3f1f9484
Felipe Vogel <fps.vogel@gmail.com>
2021-12-25 10:12:48 -0500

    Add explanatory comment to Like and Dislike models

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,3 +1,5 @@
+# Like and Dislike are identical. the alternative to this duplication is to combine them both into a Reaction model, which has a "like" boolean column to distinguish likes from dislikes. that would require a bit more querying to get just likes or just dislikes, so for now I went with this duplicating approach. in the future if more reactions are added, or if more functionality is added into them, it might be better to combine them into Reaction to prevent the duplication from growing too much.
+
 class Like < ApplicationRecord
   belongs_to :paragraph
   belongs_to :user
