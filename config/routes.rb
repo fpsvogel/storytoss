e99a70d709af8cd3926cc8762eaeed2fcb556f59
Felipe Vogel <fps.vogel@gmail.com>
2021-12-28 16:26:04 -0500

    Clean up links

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,9 @@ Rails.application.routes.draw do
   post   'login' , to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
-  get 'stories/:id', to: 'stories#show'
+  get 'stories/:id', to: 'stories#show', as: :show_story
 
+  post 'paragraphs/:id/continue', to: 'paragraphs#create', as: :create_paragraph
   post 'paragraphs/:id/like'   , to: 'paragraphs#like'   , as: :like_paragraph
   post 'paragraphs/:id/dislike', to: 'paragraphs#dislike', as: :dislike_paragraph
 end
